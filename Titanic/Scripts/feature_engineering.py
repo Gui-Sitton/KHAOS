560c9c0df42bd5b1fedc341cd1312577e56c6201
Guilherme Sitton <gsittonc@gmail.com>
2024-10-11 11:52:29 -0300

    Acabei Pré-processamento e comecei ML

--- Titanic/Scripts/feature_engineering.py
+++ Titanic/Scripts/feature_engineering.py
@@ -31,6 +31,10 @@ def engineer_features (df):
     df['is_male'] = df['sex_male'].astype(int)
     df = df.drop(['sex_male'], axis=1)
 
+    df['embarked_s'] = df['embarked_s'].astype(int)
+    df['embarked_q'] = df['embarked_q'].astype(int)
+    df['embarked_c'] = df['embarked_c'].astype(int)
+    df = df.drop(['embarked_unk'], axis=1)
 
     # Criar uma nova coluna 'Cabin_Group' que extrai a primeira letra da cabine
     def get_cabin_group(cabin):
@@ -40,4 +44,24 @@ def engineer_features (df):
 
     df['cabin_group'] = df['cabin'].apply(get_cabin_group)
 
+
+    df['title'] = df['name'].str.extract(r',\s*([^\.]+)\.', expand=False)
+    df['last_name'] = df['name'].str.extract(r'([^,]+),', expand=False)
+    df['name'] = df['name'].str.extract(r',\s*[^\s]+\s*(.*)', expand=False)
+
+    df['family_size'] = df['sibsp'] + df['parch'] + 1
+
+    df['fare_group'] = pd.cut(df['fare'], 
+                          bins=[0, 7.9104, 14.4542, 31.0000, df['fare'].max()], 
+                          labels=['Low Fare', 'Medium-Low Fare', 'Medium-High Fare', 'High Fare'])
+    
+
+    df['is_alone'] = (df['family_size'] == 1).astype(int)
+
+    df['ticket_length'] = df['ticket'].apply(len)
+
+    df['ticket_qtd'] = df.groupby('ticket')['ticket'].transform('count')
+
+    df['sex_pclass_interaction'] = df['is_male'].apply(lambda x: 'male' if x == 1 else 'female') + '_' + df['pclass'].astype(str)
+    df.columns = df.columns.str.lower()
     return df
--- Titanic/Scripts/preprocessing.py
+++ Titanic/Scripts/preprocessing.py
@@ -9,16 +9,16 @@ def preprocess_data(df):
     df['Age'] = df.groupby(['Pclass', 'Parch'])['Age'].transform(lambda x: x.fillna(x.mean()))
 
 
-    # One-Hot Encoding para sexo
+    # One-Hot Encoding para sexo 
     df = pd.get_dummies(df, columns=['Sex'], drop_first=True)
-
+    
     # Preencher valores ausentes na coluna Cabin com 'UNK'
     df['Cabin'] = df['Cabin'].fillna('Unk')
 
 
-    # Preencher valores ausentes na coluna Embarked com 'UNK'
+    # Preencher valores ausentes na coluna Embarked com 'UNK' e ohe
     df['Embarked'] = df['Embarked'].fillna('Unk')
-
+    df = pd.get_dummies(df, columns=['Embarked'], prefix='embarked', drop_first=False)
     #Colunas em lower case
     df.columns = df.columns.str.lower()
 
